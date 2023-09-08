065ca38569e561ad4c4de5762df375069ad1a4e8
Anzor Dotkulov <dotcool.a@gmail.com>
2023-09-08 15:31:45 +0300

    changeTask1

--- Task/Program.cs
+++ Task/Program.cs
@@ -14,11 +14,11 @@ if (answer == "a") {
 }
 string[] array2 = new string[array.Length];
 void SecondArrayWithIF(string[] array, string[] array2) {
-    int count = 0;
+    int temp = 0;
     for (int i = 0; i < array.Length; i++) {
         if(array[i].Length <= 3) {
-            array2[count] = array[i];
-            count++;
+            array2[temp] = array[i];
+            temp++;
         }
     }
 }
